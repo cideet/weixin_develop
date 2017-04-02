91452e279256ca00adb80c5e73d63d7093c04fe6
张三丰 <123456@qq.com>
2017-04-02 10:00:07 +0800

    接入微信公众账号API

--- test1.php
+++ test1.php
@@ -5,3 +5,24 @@
  * Date: 2017/4/2
  * Time: 3:11
  */
+
+//1、将timestamp,nonce,token按字典序排序
+$timestamp = $_GET["timestamp"];
+$nonce = $_GET["nonce"];
+$token = "vdouw_study_weixin_develop";
+$signature = $_GET["signature"];
+$echostr = $_GET["echostr"];
+
+//形成数组，然后按字典序排序
+$array = array();
+$array = array($timestamp, $nonce, $token);
+sort($array);
+
+$str = sha1(implode($array));
+if ($str == $signature && $echostr) {
+    //第一次接入微信API接口的时候
+    echo $echostr;
+    exit;
+}
+
+?>
